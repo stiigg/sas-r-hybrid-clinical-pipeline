492ab92bd42003fc708eb10615abd57614262653
Christian Baghai <christian.baghai@outlook.fr>
2025-12-16 03:23:17 +0100

    Remove redundant execution script (run_pipeline.sh)

--- run_pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-Rscript run_all.R \
-  --pipeline_mode "${PIPELINE_MODE:-dev}" \
-  --data_cut "${DATA_CUT:-}" \
-  --target_tlfs "${TARGET_TLFS:-}" \
-  --changed_sdtm "${CHANGED_SDTM:-}" \
-  --changed_adam "${CHANGED_ADAM:-}"
